6d3be7e84965d299e2399c2ccfe764e522853577
Flux <jaykonjustin@gmail.com>
2026-04-25 15:29:46 -0400

    Add files via upload

---
+++ jayhub.lua
@@ -0,0 +1,6 @@
+-- Jay Hub Loader
+-- Script ejecutable en Roblox
+
+local url = "https://c80582e5-d7db-4252-a517-3bf02b13f89e-00-d507ykys2pjs.picard.replit.dev/api/Jaythebestscript"
+
+loadstring(game:HttpGet(url))()
